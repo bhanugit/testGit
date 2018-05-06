965f7a30b800213a03cb4942d51210f857917270
RohanShreya <mbhanukiran@yahoo.com>
2018-05-05 22:42:49 -0500

    first commit

---
+++ myrest/src/main/java/com/bhanu/rest/myrest/MyrestApplication.java
@@ -0,0 +1,12 @@
+package com.bhanu.rest.myrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrestApplication.class, args);
+	}
+}
